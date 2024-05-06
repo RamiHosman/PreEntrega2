b8ae8123b7049de3e020da1cd2ccce8e307f36c6
RamiHosman <ramirohosman@gmail.com>
2024-05-06 03:47:25 -0300

    Primer commit

---
+++ app/data.js
@@ -0,0 +1,76 @@
+const mates = [
+  {
+    nombre: "Mate Tradicional de Calabaza",
+    precio: 20.99,
+    marca: "MateArg",
+    color: "Marrón",
+  },
+  {
+    nombre: "Mate de Acero Inoxidable",
+    precio: 35.5,
+    marca: "InoxMate",
+    color: "Plateado",
+  },
+  {
+    nombre: "Mate de Cerámica Pintado a Mano",
+    precio: 28.75,
+    marca: "ArteMate",
+    color: "Azul y Blanco",
+  },
+  {
+    nombre: "Mate de Madera Tallada",
+    precio: 42.99,
+    marca: "MaderaMate",
+    color: "Marrón Oscuro",
+  },
+];
+
+const termos = [
+  {
+    nombre: "Termo Clásico de Acero Inoxidable",
+    precio: 45.99,
+    marca: "ThermoSteel",
+    capacidad: "1 litro",
+  },
+  {
+    nombre: "Termo de Viaje con Pico Vertedor",
+    precio: 32.5,
+    marca: "TravelMate",
+    capacidad: "750 ml",
+  },
+  {
+    nombre: "Termo de Doble Pared de Plástico",
+    precio: 22.75,
+    marca: "EcoTherm",
+    capacidad: "500 ml",
+  },
+  {
+    nombre: "Termo de Vidrio con Funda Protectora",
+    precio: 55.99,
+    marca: "GlassGuard",
+    capacidad: "750 ml",
+  },
+];
+
+const bombillas = [
+  {
+    nombre: "Bombilla de Acero Inoxidable Cuchara",
+    precio: 8.99,
+    material: "Acero Inoxidable",
+  },
+  {
+    nombre: "Bombilla de Aluminio Recta",
+    precio: 6.5,
+    material: "Aluminio",
+  },
+  {
+    nombre: "Bombilla de Bronce Tallada",
+    precio: 12.75,
+    material: "Bronce",
+  },
+  {
+    nombre: "Bombilla de Alpaca Cuchara",
+    precio: 4.99,
+    material: "Plástico",
+  },
+];
